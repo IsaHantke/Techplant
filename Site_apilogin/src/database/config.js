4f584006085792a08aa639ff3e646182fe30abd8
Mateusdcsouza23 <mateusdocarmo@667.com>
2022-05-05 15:44:33 -0300

    Site atualizado 100%

--- Site_apilogin/src/database/config.js
+++ Site_apilogin/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "aluno",
     database: "techplant",
-    password: "0907Quiel@",
+    password: "sptech",
 };
 
 function executar(instrucao) {
